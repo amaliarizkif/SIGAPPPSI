be0741605ad14ed6c64e66997bdf526a8ca00bb1
amalia rizki febriyani <amalia.febriyanti@sigma.co.id>
2020-09-18 21:16:13 +0700

    initial

---
+++ application/views/V_header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Amoeba Bootstrap Template - Home</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url();?>assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url();?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Lato:400,300,700,900" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Amoeba - v2.0.0
+  * Template URL: https://bootstrapmade.com/free-one-page-bootstrap-template-amoeba/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container">
+
+      <div class="logo float-left">
+        <h1 class="text-light"><a href="index.html"><span>Amoeba</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="<?php echo base_url();?>assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#header">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End #header -->
---
+++ application/views/V_login.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>SIGAP</title>
+	<meta content="" name="descriptison">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="<?php echo base_url();?>assets/img/favicon.png" rel="icon">
+	<link href="<?php echo base_url();?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Lato:400,300,700,900" rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
+
+	<style type="text/css">	</style>
+
+</head>
+<!-- ======= Hero Section ======= -->
+<section id="hero" style="height: 100vh;">
+	<div class="hero-container" style="padding-top: 0px;">
+		<h1>Login to SIGAP</h1>
+		<h2 style="font-weight: 550; text-align: center;">Sistem Pelaporan Kesehatan pada Operasi Matra Gabungan Mabes TNI</h2>
+		<div class="card">
+			<div class="card-body">
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+						
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					</div>
+
+					<button style="float: right;" type="submit" class="btn-get-started" >Submit</button>
+				</form>
+
+			</div>
+			<!-- <a href="#about" class="btn-get-started scrollto">Get Started</a> -->
+		</div>
+
+		
+	</div>
+</section><!-- #hero -->
+
+
+<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url();?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url();?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="<?php echo base_url();?>assets/vendor/php-email-form/validate.js"></script>
+<script src="<?php echo base_url();?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="<?php echo base_url();?>assets/vendor/venobox/venobox.min.js"></script>
+
+<!-- Template Main JS File -->
+<!-- <script src="<?php echo base_url();?>assets/js/main.js"></script> -->
+
+</body>
+
+</html>
